0ae94afa9bf8fe1b54434221b70d0a81c0e3d820
i-ashy <ashleymelinares@gmail.com>
2024-07-24 23:59:50 -0600

    iterando json y estilos

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,44 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React from "react";
+import estilos from './page.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import datos from "./jsn2.json";
 
-export default function Home() {
+const jsonIterando = () => {
+  console.log(datos);
+
+  if (!Array.isArray(datos)) {
+    return <div>Error: datos no es un array</div>;
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    //container
+    <>
+    <div className={`container w-75 ${estilos.pers}`}>
+      <h1 className="mt-3 mb-3 text-center">Fraudes Cripto</h1>
+      {datos.map((dato) => (
+        <div className="card mb-3" key={dato.id}>
+          <div className="card-header">
+            <h2 className="card-title">{dato.titulo}</h2>
+          </div>
+          <div className="row card-body">
+            <div className="col-md-2 d-flex flex-column justify-content-center">
+              <img className="img-fluid img-thumbnail" src={dato.imagen_de_referencia}></img><br></br>
+            </div>
+            <div className="col-md">
+              <p><b>Descripción:</b> {dato.descripcion}</p>
+              <p><b>Características:</b></p>
+              <ul>
+                {dato.caracteristicas.map((caracteristica) => (
+                  <li key={caracteristica.id}>{caracteristica}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      ))}
+    </div>
+    </>
   );
 }
+
+export default jsonIterando;
